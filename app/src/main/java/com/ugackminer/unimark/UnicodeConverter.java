16b1dc9d9b599df811d382d05a64be5f7e9d44c5
ugackMiner <ugackMiner@gmail.com>
2023-11-05 21:46:56 -0500

    Add monospace converter, scaffold strikethrough and cursive, and improve Unicode checking on underline.

--- app/src/main/java/com/ugackminer/unimark/UnicodeConverter.java
+++ app/src/main/java/com/ugackminer/unimark/UnicodeConverter.java
@@ -44,13 +44,54 @@ public class UnicodeConverter {
         return builder.toString();
     }
 
+    /**
+     * Takes in a String and converts all letters
+     * to be in the {@code MATHEMATICAL MONOSPACE} Unicode range ({@code U+1D670}/{@code U+1D7F6})
+     * @param text The input string
+     * @return The monospaced version of the string
+     */    
+    public static String convertToMonospace(String text) {
+        StringBuilder builder = new StringBuilder(text.length());
+
+        for (char character : text.toCharArray()) {
+            if (Character.isAlphabetic(character)) {
+                builder.append(Character.toChars(character + (0x1D629 + (Character.isUpperCase(character) ? 6 : 0)))); // 1D62F uppercase vs 1D629 lowercase
+            } else if (Character.isDigit(character)) {
+                builder.append(Character.toChars(character + 0x1D7C6));
+            } else {
+                builder.append(character);
+            }
+        }
+
+        return builder.toString();
+    }
+
+    
+    public static String convertToCursive(String text) {
+        return text;
+    }
+
+
+    /**
+     * Takes in a string and puts {@code U+035F} after every alphanumeric character
+     * in order to make an underlined string.
+     * @param text The input string
+     * @return The underlined version of the string
+     */
     public static String convertToUnderline(String text) {
         StringBuilder builder = new StringBuilder(text.length()*2);
 
         for (char character : text.toCharArray()) {
             builder.append(character);
-            builder.append("\u035F");
+            if (Character.UnicodeBlock.of(character) == Character.UnicodeBlock.BASIC_LATIN) {
+                builder.append("\u035F");
+            }
         }
         return builder.toString();
+    }
+
+
+    public static String convertToStrikethrough(String text) {
+        return text;
     }
 }
--- app/src/test/java/com/ugackminer/unimark/test/UnicodeConverterTest.java
+++ app/src/test/java/com/ugackminer/unimark/test/UnicodeConverterTest.java
@@ -10,21 +10,30 @@ public class UnicodeConverterTest {
     static final String UPPERCASE_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     static final String LOWERCASE_STRING = "abcdefghijklmnopqrstuvwxyz";
     static final String NUMERICAL_STRING = "0123456789";
+    static final String SYMBOL_STRING = ".?!@#$%^&*()/❤︎😊";
 
     @Test void boldConversionTest() {
         assertEquals("𝗔𝗕𝗖𝗗𝗘𝗙𝗚𝗛𝗜𝗝𝗞𝗟𝗠𝗡𝗢𝗣𝗤𝗥𝗦𝗧𝗨𝗩𝗪𝗫𝗬𝗭", UnicodeConverter.convertToBold(UPPERCASE_STRING));
         assertEquals("𝗮𝗯𝗰𝗱𝗲𝗳𝗴𝗵𝗶𝗷𝗸𝗹𝗺𝗻𝗼𝗽𝗾𝗿𝘀𝘁𝘂𝘃𝘄𝘅𝘆𝘇", UnicodeConverter.convertToBold(LOWERCASE_STRING));
         assertEquals("𝟬𝟭𝟮𝟯𝟰𝟱𝟲𝟳𝟴𝟵", UnicodeConverter.convertToBold(NUMERICAL_STRING));
+        assertEquals(SYMBOL_STRING, SYMBOL_STRING);
     }
 
     @Test void italicConversionTest() {
         assertEquals("𝘈𝘉𝘊𝘋𝘌𝘍𝘎𝘏𝘐𝘑𝘒𝘓𝘔𝘕𝘖𝘗𝘘𝘙𝘚𝘛𝘜𝘝𝘞𝘟𝘠𝘡", UnicodeConverter.convertToItalic(UPPERCASE_STRING));
         assertEquals("𝘢𝘣𝘤𝘥𝘦𝘧𝘨𝘩𝘪𝘫𝘬𝘭𝘮𝘯𝘰𝘱𝘲𝘳𝘴𝘵𝘶𝘷𝘸𝘹𝘺𝘻", UnicodeConverter.convertToItalic(LOWERCASE_STRING));
+        assertEquals(SYMBOL_STRING, SYMBOL_STRING);
     }
 
+
+
+    // Test does not currently work. Maybe the block we check for in the underline thing doesn't include punctuation?
     @Test void underlineConversionTest() {
         assertEquals("A͟B͟C͟D͟E͟F͟G͟H͟I͟J͟K͟L͟M͟N͟O͟P͟Q͟R͟S͟T͟U͟V͟W͟X͟Y͟Z͟", UnicodeConverter.convertToUnderline(UPPERCASE_STRING));
         assertEquals("a͟b͟c͟d͟e͟f͟g͟h͟i͟j͟k͟l͟m͟n͟o͟p͟q͟r͟s͟t͟u͟v͟w͟x͟y͟z͟", UnicodeConverter.convertToUnderline(LOWERCASE_STRING));
         assertEquals("0͟1͟2͟3͟4͟5͟6͟7͟8͟9͟", UnicodeConverter.convertToUnderline(NUMERICAL_STRING));
+        assertEquals(".͟?͟!͟@͟#͟$͟%͟^͟&͟*͟(͟)͟/͟❤︎😊", SYMBOL_STRING);
     }
+
+
 }
